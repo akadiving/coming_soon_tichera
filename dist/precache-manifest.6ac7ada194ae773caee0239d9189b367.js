a9662bf268a9e53ec2c813558f54a9a43712bef5
akadiving <akoivanishvili6@gmail.com>
2021-01-12 15:00:40 +0400

    change favicon

---
+++ dist/precache-manifest.6ac7ada194ae773caee0239d9189b367.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e080cf12e0004190e3",
+    "url": "/css/app.2580818e.css"
+  },
+  {
+    "revision": "bbb8dcac9ad9646201ecf7ee4469fd6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7e080cf12e0004190e3",
+    "url": "/js/app.f04ed9f1.js"
+  },
+  {
+    "revision": "6731f1472eef35ededfc",
+    "url": "/js/chunk-vendors.015c80cb.js"
+  },
+  {
+    "revision": "e31e0459e338b62f743503c758066a84",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <container>
+      <p>Tichera</p>
+      <h2>COMING SOON</h2>
+    </container>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
